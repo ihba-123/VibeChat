170a7ceaef344abc8639fec44479b70545722c6a
abhishek <abhishekbhatta110@gmail.com>
2025-11-14 10:57:14 +0545

    requirement.txt pushed

--- Backend/System/chatapp/middleware.py
+++ Backend/System/chatapp/middleware.py
@@ -32,30 +32,14 @@ def get_user_from_token(token: str):
 
 
 class JWTAuthMiddleware(BaseMiddleware):
-    """
-    Extracts JWT from:
-        - query string:  ?token=<jwt>
-        - OR cookie:      access_token=<jwt>   (uncomment the cookie block if you prefer)
-    Sets scope['user'] to the real User or AnonymousUser.
-    """
 
     def __init__(self, inner):
         super().__init__(inner)
 
     async def __call__(self, scope, receive, send):
-        # ---------- 1. FROM QUERY STRING ----------
+
         query = parse_qs(scope.get("query_string", b"").decode())
         token = query.get("token", [None])[0]
-
-        # ---------- 2. (OPTIONAL) FROM COOKIE ----------
-        # headers = dict(scope["headers"])
-        # if b"cookie" in headers:
-        #     from http.cookies import SimpleCookie
-        #     cookie = SimpleCookie()
-        #     cookie.load(headers[b"cookie"].decode())
-        #     token = cookie.get("access_token").value if cookie.get("access_token") else token
-
-        # ---------- 3. SET USER ----------
         scope["user"] = await get_user_from_token(token) if token else AnonymousUser()
 
         return await super().__call__(scope, receive, send)
